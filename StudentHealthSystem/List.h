07ca5ac0e95e8c8317f097efcb108faab6c42210
yaoMYZ <mozhenyaoMYZ@gmail.com>
2016-09-28 17:21:34 +0800

    Version2.0

--- StudentHealthSystem/List.h
+++ StudentHealthSystem/List.h
@@ -4,15 +4,15 @@
 #include<iostream>
 using namespace std;
 
-template <class T> 
+template <class T>
 class List
 {
 private:
 	T m_Member;
-	List *m_Head;
-	List *m_Tail;
-	List *m_Right;
-	List *m_Left;
+	List<T> *m_Head;
+	List<T> *m_Tail;
+	List<T> *m_Right;
+	List<T> *m_Left;
 
 public:
 	List(){
@@ -22,23 +22,28 @@ public:
 		m_Left = NULL;
 	}
 
+
 	void Push(T);
 	void Show();
 	void Pop();
 	void Reverse();
-	void Insert(int,T);
+	bool Insert(int, T);
+	bool Delete(int pos);
+	int Length();
+	bool IsEmpty();
+	T &Member(int pos);
+	bool Replace(int pos, T member);
 };
 
 
 template <class T>//模板类的声明和定义一定要在同一文件中
 void List<T>::Push(T member)
 {
-	List *tail, *temp;
+	List<T> *tail, *temp;
 	temp = new List;
 	temp->m_Member = member;
 	if (m_Head == NULL){
-		List *head = new List;
-		head->m_Member = (T)0;
+		List<T> *head = new List;
 		m_Head = head;
 
 	}
@@ -52,10 +57,9 @@ void List<T>::Push(T member)
 }
 
 
-
 template <class T>//模板类的声明和定义一定要在同一文件中
 void List<T>::Show(){
-	List *p;
+	List<T> *p;
 	p = m_Head->m_Right;
 	while (p != NULL){
 		cout << p->m_Member << " ";
@@ -68,7 +72,7 @@ void List<T>::Show(){
 
 template <class T>//模板类的声明和定义一定要在同一文件中
 void List<T>::Pop(){
-	List *pop,tail;
+	List<T> *pop, tail;
 	pop = m_Tail;
 	tail = pop->m_Left;
 	delete pop;
@@ -80,10 +84,11 @@ void List<T>::Pop(){
 
 template <class T>//模板类的声明和定义一定要在同一文件中  
 void List<T>::Reverse(){
-	List *now, *p,*temp;
+	List<T> *now, *p, *temp;
 	now = m_Head->m_Right;
 	p = now->m_Right;
 	now->m_Right = NULL;
+	m_Tail = now;//the end of list is now the first one of the list
 	while (p != NULL){
 		temp = p;
 		p = p->m_Right;
@@ -99,26 +104,109 @@ void List<T>::Reverse(){
 
 
 template <class T>//模板类的声明和定义一定要在同一文件中  
-void List<T>::Insert(int munber,T member){
-	List *p = m_Head;
-	
-	int i = 0;
-	while (i<munber){
+bool List<T>::Insert(int number, T member){
+	int len = Length();
+	if (number<1 || number>len + 1) return false;
+
+	List<T> *newNode = new List;//create an new node
+	newNode->m_Member = member;
+
+	if (number == len + 1){//element insert in the end of the list
+		m_Tail->m_Right = newNode;
+		newNode->m_Left = m_Tail;
+		m_Tail = newNode;
+		m_Tail->m_Right = NULL;
+		return true;
+	}
+
+	List<T> *p = m_Head;
+
+	int i = 1;
+	while (i<number){
 		p = p->m_Right;
 		i++;
 	}
-	List *pRight = p->m_Right;
+	List<T> *pRight = p->m_Right;
 
-	List *newNode = new List;
-	newNode->m_Member = member;
-	
 	p->m_Right = newNode;
 	newNode->m_Left = p;
 
-	if (pRight!=NULL)
-	pRight->m_Left = newNode;
+	if (pRight != NULL)
+		pRight->m_Left = newNode;
 	newNode->m_Right = pRight;
+
+	return true;
 }
 
+template <class T>//模板类的声明和定义一定要在同一文件中
+bool List<T>::Delete(int pos){
+	int len = Length();
+	if (pos<1 || pos>len){
+		cout << "输入超界！" << endl;
+		return false;
+	}
 
+	List<T> *del = NULL;
+	if (pos == len){//when delete the final element in the list 
+		del = m_Tail;
+		m_Tail = del->m_Left;
+		m_Tail->m_Right = NULL;
+		delete del;
+		return true;
+	}
+
+	List<T> *p = m_Head;
+	for (int i = 1; i < pos; i++){
+		p = p->m_Right;
+	}
+	del = p->m_Right;
+	p->m_Right = del->m_Right;
+	del->m_Right->m_Left = p;
+	delete del;
+	return true;
+}
+
+template <class T>//模板类的声明和定义一定要在同一文件中
+int List<T>::Length(){
+	int len = 0;
+	List<T> *p = m_Head;
+	while (p->m_Right != NULL){
+		p = p->m_Right;
+		len++;
+	}
+	return len;
+}
+
+template<class T>
+bool List<T>::IsEmpty(){
+	if (m_Head == NULL)
+		return true;
+	else
+		return false;
+}
+
+template<class T>
+T & List<T>::Member(int pos){
+	if (pos > Length()) {
+		cout << "out of range!" << endl;
+		exit(0);
+	}
+	List<T> *p = m_Head;
+	for (int i = 0; i < pos; i++){
+		p = p->m_Right;
+	}
+	return p->m_Member;
+}
+
+template<class T>
+bool List<T>::Replace(int pos, T member){
+	int len = Length();
+	if (pos<1 || pos>len){
+		cout << "输入超界！" << endl;
+		return false;
+	}
+
+	Member(pos) = member;
+	return true;
+}
 #endif
--- StudentHealthSystem/StudentHealth.h
+++ StudentHealthSystem/StudentHealth.h
@@ -4,6 +4,7 @@
 #include<fstream>
 #include"stdio.h"
 #include"stdlib.h"
+#include"List.h"
 using namespace std;
 
 
@@ -15,6 +16,13 @@ struct Student
 	char strBirthday[11];
 	char IfHealthy[5];
 
+	Student(){
+		strcpy_s(strName, "0");
+		strcpy_s(strSex, "0");
+		strcpy_s(strStudentID, "0");
+		strcpy_s(strBirthday, "0");
+		strcpy_s(IfHealthy, "0");
+	}
 	Student & operator=(Student other){
 		strcpy_s(strName, other.strName);
 		strcpy_s(strSex, other.strSex);
@@ -23,24 +31,64 @@ struct Student
 		strcpy_s(IfHealthy, other.IfHealthy);
 		return *this;
 	}
+
+	friend bool operator<(Student &s1, Student &s2);
+	friend ostream & operator<<(ostream & output, Student st);
+	friend istream & operator>>(istream & input, Student &st);
 };
+
+bool operator<(Student &s1, Student &s2){
+	if (strcmp(s1.strStudentID, s2.strStudentID)<0){
+		return true;
+	}
+else
+	return false;
+}
+
+ostream & operator<<(ostream & output, Student st){
+	cout << "名字：" << st.strName << endl;
+	cout << "性别：" << st.strSex << endl;
+	cout << "学号：" << st.strStudentID << endl;
+	cout << "生日：" << st.strBirthday << endl;
+	cout << "是否健康：" << st.IfHealthy << endl;
+	cout << endl << endl;
+	return output;
+}
+
+istream & operator>>(istream & input, Student &st){//st must pass address
+	int judeHealth = 1;
+	cout << "请输入姓名：";
+	input >> st.strName;
+	cout << "请输入性别：";
+	input >> st.strSex;
+	cout << "请输入学号：";
+	input >> st.strStudentID;
+	cout << "请输入生日（格式如：1998/10/8)：";
+	input >> st.strBirthday;
+	cout << "请输入健康状况（不健康输入0，健康输入非0字符）：";
+	input >> judeHealth;
+	if (judeHealth == 0)
+		strcpy_s(st.IfHealthy, "否");
+	else
+		strcpy_s(st.IfHealthy, "是");
+	cout << endl;
+	return input;
+}
 
 
 class StudentHealth{
 private:
-	Student *SHlist;
-	int index;
-	int MaxSize;
-
-	void JudeIfOverflow(){
-		if (index >= MaxSize){//Jude data overflow
-			Student *newList = new Student[MaxSize * 2];//To apply for a bigger array
-			memmove(newList, SHlist, sizeof(Student)*MaxSize);
-			MaxSize *= 2;
-			delete[] SHlist;
-			SHlist = newList;//variable SHlist point to the variable newList
-		}
-	}
+	List<Student> SHlist;
+	int Length;
+	//void JudeIfOverflow(){
+	//	if (Length >= MaxSize){//Jude data overflow
+	//		Student *newList = new Student[MaxSize * 2];//To apply for a bigger array
+	//		memmove(newList, SHlist, sizeof(Student)*MaxSize);
+	//		MaxSize *= 2;
+	//		delete[] SHlist;
+	//		SHlist = newList;//variable SHlist point to the variable newList
+	//	}
+	//}
 
 	//jude if the length of the array overflow
 	bool IfLenOutOfRange(char a[]){
@@ -88,16 +136,12 @@ private:
 	//write the data to the file
 	void Write(){
 		ofstream write("Information.dat", ios::binary|ios::out);
-		for (int i = 0; i < index; i++){
-			write.write((char*)&SHlist[i], sizeof(SHlist[i]));
+		for (int i = 1; i <= Length;i++){
+			write.write((char*)&SHlist.Member(i), sizeof(Student));
 		}
 		write.close();
 	}
 	void Initial(){
-		index = 0;
-		MaxSize = 100;
-		if (SHlist == NULL)
-			SHlist = new Student[MaxSize];
 		ifstream read("Information.dat", ios::binary|ios::in);
 		int l, m;
 		l = read.tellg();
@@ -105,10 +149,12 @@ private:
 		m = read.tellg();
 		read.seekg(0, ios::beg);//move the file point back to the head of the file
 		int len = (m - l) / sizeof(Student);//get the number of the Student structrue in the file
-		for (int i = 0; i < len; i++){
-			read.read((char*)&SHlist[i], sizeof(SHlist[i]));
+		Length = len;
+		for (int i = 1; i <= Length; i++){
+			Student temp;
+			read.read((char*)&temp, sizeof(Student));
+			SHlist.Push(temp);
 			//add the charater '\0' to every element of SHlist[i]
-			index++;
 		}
 		read.close();
 	}
@@ -116,13 +162,11 @@ private:
 
 public:
 	StudentHealth(){
-		SHlist = NULL;
 		Initial();
 	}
 
 	~StudentHealth(){
-		if (SHlist != NULL)
-			delete[] SHlist;
+
 	}
 
 	void Menu(){
@@ -193,53 +237,11 @@ public:
 		cout << "请输入学生人数：";
 		int number = 0;
 		cin >> number;
-		int judeHealth = 1;
 		for (int i = 0; i < number; i++){
-			bool bJudeRange=false;
-			cout << "请输入姓名：";
-			cin >> SHlist[i].strName;
-			bJudeRange=IfLenOutOfRange(SHlist[i].strName);
-			if (bJudeRange){
-			   fflush(stdout);
-			   system("cls");
-			   i--;
-			   continue;
-			}
-			cout << "请输入性别：";
-			cin >> SHlist[i].strSex;
-			bJudeRange=IfLenOutOfRange(SHlist[i].strSex);
-			if (bJudeRange){
-				fflush(stdout);
-               system("cls");
-			   i--;
-			   continue;
-			}
-			cout << "请输入学号：";
-			cin >> SHlist[i].strStudentID;
-			bJudeRange=IfLenOutOfRange(SHlist[i].strStudentID);
-			if (bJudeRange){
-				fflush(stdout);
-               system("cls");
-			   i--;
-			   continue;
-			}
-			cout << "请输入生日（格式如：1998/10/8)：";
-			cin >> SHlist[i].strBirthday;
-			bJudeRange=IfLenOutOfRange(SHlist[i].strBirthday);
-			if (bJudeRange){
-			   fflush(stdout);
-			   system("cls");
-			   i--;
-			   continue;
-			}
-			cout << "请输入健康状况（不健康输入0，健康输入非0字符）：";
-			cin >> judeHealth;
-			if (judeHealth == 0)
-				strcpy_s(SHlist[i].IfHealthy, "否");
-			else
-				strcpy_s(SHlist[i].IfHealthy, "是");
-			index++;
-			cout << endl;
+			Student temp;
+			cin >> temp;
+			SHlist.Push(temp);
+			Length++;
 		}
 		Write();
 		cout << "输入结束！" << endl;
@@ -249,99 +251,27 @@ public:
 
 
 	void AddStudent(){
-		JudeIfOverflow();//Jude data overflow
-		bool bJudeRange=false;
-		int judeHealth = 1;
-		cout << "请输入姓名：";
-		cin >> SHlist[index].strName;
-		bJudeRange=IfLenOutOfRange(SHlist[index].strName);
-		if (bJudeRange){
-		   system("cls");
-		   AddStudent();
-		}
-		cout << "请输入性别：";
-		cin >> SHlist[index].strSex;
-		bJudeRange=IfLenOutOfRange(SHlist[index].strSex);
-		if (bJudeRange){
-		   system("cls");
-		   AddStudent();
-		}
-		cout << "请输入学号：";
-		cin >> SHlist[index].strStudentID;
-		bJudeRange=IfLenOutOfRange(SHlist[index].strStudentID);
-		if (bJudeRange){
-		   system("cls");
-		   AddStudent();
-		}
-		cout << "请输入生日（格式如：1998/10/8)：";
-		cin >> SHlist[index].strBirthday;
-		bJudeRange=IfLenOutOfRange(SHlist[index].strBirthday);
-		if (bJudeRange){
-		   system("cls");
-		   AddStudent();
-		}
-		cout << "请输入健康状况（不健康输入0，健康输入非0字符）：";
-		cin >> judeHealth;
-		if (judeHealth == 0)
-			strcpy_s(SHlist[index].IfHealthy, "否");
-		else
-			strcpy_s(SHlist[index].IfHealthy, "是");
-		index++;
+		Student temp;
+		cin >> temp;
+		SHlist.Push(temp);
+		Length++;
 		Write();
 		system("pause");
 		Menu();
 	}
 
 	void InsertStudent(){
-		JudeIfOverflow();//Jude data overflow
 		int pos = 0;
-		cout << "请输入插入位置（输入0插入到第一个位置，以此类推）：";
+		cout << "请输入插入位置（输入1插入到第一个位置，以此类推）：";
 		cin >> pos;
-		if (pos > index){
+		if (pos > Length+1||pos<1){
 			cout << "超过界限请重新输入！";
 			InsertStudent();
 		}
-		for (int i = index - 1; i >= pos; i--){//all elements after the index of pos move backward one byte
-			SHlist[i + 1] = SHlist[i];
-		}
-		index++;
-
-		bool bJudeRange=false;
-		int judeHealth = 1;
-		cout << "请输入姓名：";
-		cin >> SHlist[pos].strName;
-		bJudeRange=IfLenOutOfRange(SHlist[pos].strName);
-		if (bJudeRange){
-		   system("cls");
-		   InsertStudent();
-		}
-		cout << "请输入性别：";
-		cin >> SHlist[pos].strSex;
-		bJudeRange=IfLenOutOfRange(SHlist[pos].strSex);
-		if (bJudeRange){
-		   system("cls");
-		   InsertStudent();
-		}
-		cout << "请输入学号：";
-		cin >> SHlist[pos].strStudentID;
-		bJudeRange=IfLenOutOfRange(SHlist[pos].strStudentID);
-		if (bJudeRange){
-		   system("cls");
-		   InsertStudent();
-		}
-		cout << "请输入生日（格式如：1998/10/8)：";
-		cin >> SHlist[pos].strBirthday;
-		bJudeRange=IfLenOutOfRange(SHlist[pos].strBirthday);
-		if (bJudeRange){
-		   system("cls");
-		   InsertStudent();
-		}
-		cout << "请输入健康状况（不健康输入0，健康输入非0字符）：";
-		cin >> judeHealth;
-		if (judeHealth == 0)
-			strcpy_s(SHlist[pos].IfHealthy, "否");
-		else
-			strcpy_s(SHlist[pos].IfHealthy, "是");
+		Student temp;
+		cin >> temp;
+		SHlist.Insert(pos, temp);
+		Length++;
 		Write();
 		system("pause");
 		Menu();
@@ -352,21 +282,19 @@ public:
 		cout << "请输入学生的学号：";
 		cin >> ID;
 		bool IfHaveID = false;
-		for (int i = 0; i < index; i++){
-			if (strcmp(ID, SHlist[i].strStudentID) == 0){
-				if (i != index - 1){//if i==index-1,we should do not thing but let index--;
-					for (int j = i; j < index - 1; j++){
-						SHlist[j] = SHlist[j + 1];
-					}
-				}
+		for (int i = 1; i <= Length; i++){
+			if (strcmp(ID, SHlist.Member(i).strStudentID) == 0){
+				SHlist.Delete(i);
 				IfHaveID = true;
-				index--;
+				Length--;
 				Write();
 				break;
 			}
 		}
-		if (!IfHaveID)
-			cout << "没有这学生！" << endl;
+		if (!IfHaveID){
+			cout << "没有这学生，请重新输入！" << endl;
+			DeleteStudent();
+		}			
 		system("pause");
 		Menu();
 	}
@@ -376,63 +304,27 @@ public:
 		cout << "请输入学生的学号：";
 		cin >> ID;
 		bool IfHaveID = false;
-		for (int i = 0; i < index; i++){
-			if (strcmp(ID, SHlist[i].strStudentID) == 0){
-				bool bJudeRange=false;
-				int judeHealth = 1;
-				cout << "请输入姓名：";
-				cin >> SHlist[i].strName;
-				bJudeRange=IfLenOutOfRange(SHlist[i].strName);
-				if (bJudeRange){
-					system("cls");
-					ChangeInformation();
-				}
-				cout << "请输入性别：";
-				cin >> SHlist[i].strSex;
-				bJudeRange=IfLenOutOfRange(SHlist[i].strSex);
-				if (bJudeRange){
-					system("cls");
-					ChangeInformation();
-				}
-				cout << "请输入学号：";
-				cin >> SHlist[i].strStudentID;
-				bJudeRange=IfLenOutOfRange(SHlist[i].strStudentID);
-				if (bJudeRange){
-					system("cls");
-					ChangeInformation();
-				}
-				cout << "请输入生日（格式如：1998/10/8)：";
-				cin >> SHlist[i].strBirthday;
-				bJudeRange=IfLenOutOfRange(SHlist[i].strBirthday);
-				if (bJudeRange){
-					system("cls");
-					ChangeInformation();
-				}
-				cout << "请输入健康状况（不健康输入0，健康输入非0字符）：";
-				cin >> judeHealth;
-				if (judeHealth == 0)
-					strcpy_s(SHlist[i].IfHealthy, "否");
-				else
-					strcpy_s(SHlist[i].IfHealthy, "是");
+		for (int i = 1; i <= Length; i++){
+			if (strcmp(ID, SHlist.Member(i).strStudentID) == 0){
+				Student temp;
+				cin >> temp;
+				SHlist.Replace(i, temp);
 				IfHaveID = true;
 				Write();
 				break;
 			}
 		}
-		if (!IfHaveID)
-			cout << "没有这学生！" << endl;
+		if (!IfHaveID){
+			cout << "没有这学生，请重新输入！" << endl;
+			ChangeInformation();
+		}
 		system("pause");
 		Menu();
 	}
 
 	void ShowStudentInformation(){
-		for (int i = 0; i < index; i++){
-			cout << "名字：" << SHlist[i].strName << endl;
-			cout << "性别：" << SHlist[i].strSex << endl;
-			cout << "学号：" << SHlist[i].strStudentID << endl;
-			cout << "生日：" << SHlist[i].strStudentID << endl;
-			cout << "是否健康：" << SHlist[i].IfHealthy << endl;
-			cout << endl << endl;
+		for (int i = 1; i <= Length; i++){
+			cout << SHlist.Member(i);
 		}
 		system("pause");
 		Menu();
@@ -443,35 +335,28 @@ public:
 		cout << "请输入学生的学号：";
 		cin >> ID;
 		bool IfHaveID = false;
-		for (int i = 0; i < index; i++){
-			if (strcmp(ID, SHlist[i].strStudentID) == 0){
-				cout << "名字：" << SHlist[i].strName << endl;
-				cout << "性别：" << SHlist[i].strSex << endl;
-				cout << "学号：" << SHlist[i].strStudentID << endl;
-				cout << "生日：" << SHlist[i].strStudentID << endl;
-				cout << "是否健康：" << SHlist[i].IfHealthy << endl;
-				cout << endl << endl;
+		for (int i = 1; i <= Length; i++){
+			if (strcmp(ID, SHlist.Member(i).strStudentID) == 0){
+				cout << SHlist.Member(i);
 				IfHaveID = true;
 				break;
 			}
 		}
-		if (!IfHaveID)
-			cout << "没有这学生！" << endl;
+		if (!IfHaveID){
+			cout << "没有这学生，请重新输入！" << endl;
+			FindStudent();
+		}
 		system("pause");
 		Menu();
 	}
 
 	void ShowInformationByID(){
-		Student *newList = new Student[index];
-		memmove(newList, SHlist, (sizeof(Student))*index);
-		Quicksort(newList, 0, index - 1);
-		for (int i = 0; i < index; i++){
-			cout << "名字：" << newList[i].strName << endl;
-			cout << "性别：" << newList[i].strSex << endl;
-			cout << "学号：" << newList[i].strStudentID << endl;
-			cout << "生日：" << newList[i].strStudentID << endl;
-			cout << "是否健康：" << newList[i].IfHealthy << endl;
-			cout << endl << endl;
+		Student *newList = new Student[Length];
+		for (int index = 1; index <= Length; index++)
+			newList[index - 1] = SHlist.Member(index);
+		Quicksort(newList, 0, Length - 1);
+		for (int i = 0; i < Length; i++){
+			cout << newList[i];
 		}
 		delete[] newList;
 		system("pause");
